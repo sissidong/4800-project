76254b2766f15764a9c2250669c73abd90b6a8b2
sissidong <dongxiaofu1130@gmail.com>
2017-12-19 00:27:23 -0600

    final oneD

--- R/oneDsample.R
+++ R/oneDsample.R
@@ -1,7 +1,6 @@
 #' Single variable Rejection Sampling
 #'
 #' This function 'oneDsample(f,N,lb,up,method)' implements single variable rejection sampling for continuous rvs with or without bounded support.
-#'
 #' Additionally, the function 'oneDsampleplot()' and 'oneDsamplehist()' shows the plot of original function and simulation density function.
 #'
 #' @param f       The pdf that we are sampling from
@@ -19,11 +18,8 @@
 #'
 #' @example
 #'
-#' f<- function(x) {ifelse(-1< x & x < 0, 2*(x+1), 0)}
+#'f<- function(x) {ifelse(-1< x & x < 0, 2*(x+1), 0)}
 #'oneDsampleplot(oneDsample(f,20000,-1,0,method='unif'))
-#'
-#'f<-function(x) {ifelse(0 <= x & x <=1, 2*(1-x), 0)}
-#'oneDsampleplot(oneDsample(f,50000))
 #'
 #'f = function(x) {ifelse(0 <= x & x <= 2*pi ,1/2/pi *(sin(x) + 1),0)}
 #'oneDsampleplot(oneDsample(f,50000,0,2*pi))
@@ -34,13 +30,6 @@
 #'f<- function(x) dnorm(x,-10,2)
 #'oneDsampleplot(oneDsample(f,method='t'))
 #'
-#'f<- function(x) 1/(pi*(1+x^2))
-#'oneDsampleplot(oneDsample(f))
-#'
-#'f<-function(x) {dunif(x,-500,500)}
-#'oneDsampleplot(oneDsample(f,2000))
-#'
-
 unif<-function(f, N=50000, lb, ub){
   maxf<-optimize(f,c(lb,ub),maximum = TRUE)
   maxf<-maxf$objective
@@ -55,7 +44,6 @@ norm<-function(f, N=50000){
   c= 2*maxf/dnorm(a,a,sd)
   data.frame(x = replicate(N, {sx <- rnorm(1,a,sd); ifelse( runif(1,0,c*dnorm(sx,a,sd)) < f(sx), sx, NA)}))
 }
-
 t<-function(f,N=50000){
   x<-runif(100000,-1000,1000)
   maxf<-max(f(x))
@@ -66,7 +54,6 @@ t<-function(f,N=50000){
   data.frame(x = replicate(N, {sx <- rt.scaled(1,1,mean=a,sd);
   ifelse( runif(1,0,c*dt.scaled(sx,df=1,mean=a,sd)) < f(sx), sx, NA)}))
 }
-
 oneDsample <- function(f, N=50000, lb=Inf, ub=Inf,method='best') {
   if (abs(integrate(f,lb,ub)$val-1)>0.001){
     stop("Error: not a pdf.The area under the function you given should be 1")
